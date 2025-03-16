bdc7ab9b046eae6b6889e8f6c27b34b07098fbf3
Adeola Adebowale <oluadebojnr@gmail.com>
2025-03-16 03:09:57 +0100

    code cleanup

--- src/components/authPageComponents/signup.tsx
+++ src/components/authPageComponents/signup.tsx
@@ -15,7 +15,7 @@ import { RootState } from "@/store";
 // Enhanced password validation schema
 const passwordSchema = z
     .string()
-    .min(8, "Password must be at least 8 characters")
+    .min(6, "Password must be at least 6 characters")
     .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
     .regex(/[a-z]/, "Password must contain at least one lowercase letter")
     .regex(/[0-9]/, "Password must contain at least one number")
